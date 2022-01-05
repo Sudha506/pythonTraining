7d5e4bb6e60c1debbf332a3b411537b776a71cca
sudha <sudhakarsds506@gmail.com>
2022-01-05 13:13:36 +0530

    adding a new python file

---
+++ default_argument_test.py
@@ -0,0 +1,15 @@
+# def add(a,b=20):
+    # return a+b 
+# my_func=add(2,50)
+# print(my_func)
+
+def add(x,a=10,b=20):
+    return x+a+b 
+my_func=add(2,3,5)
+print(my_func)
+
+# def add(a=10,b):   default arguments must be passed in last only.
+    # return a+b 
+# my_func=add(2,50)
+# print(my_func)
+
